de22b261451b4cbd28b46f59695a39f1b77c1223
pingestremo86 <ivanalvino@libero.it>
2021-11-03 15:56:10 +0100

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,11 @@ import { HelloComponent } from './hello.component';
 import { TestComponent } from '../test/test.component';
 import { FirstComponent } from './first/first.component';
 import { CeoComponent } from './ceo/ceo.component';
+import { DirComponent } from './dir/dir.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, TestComponent, FirstComponent, CeoComponent ],
+  declarations: [ AppComponent, HelloComponent, TestComponent, FirstComponent, CeoComponent, DirComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
